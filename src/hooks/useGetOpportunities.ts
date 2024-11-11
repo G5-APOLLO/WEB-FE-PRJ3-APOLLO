900f552a86065ccbaa04674173efc12f39fdffbf
Maria Del Toro <Mdeltoroa@uninorte.edu.co>
2024-11-11 17:13:45 -0500

    fix hook useGetOpportunities.ts

--- src/hooks/useGetOpportunities.ts
+++ src/hooks/useGetOpportunities.ts
@@ -5,6 +5,6 @@ export const useGetOpportunities = (clientId?: number) => {
   return useQuery({
     queryKey: ['opportunities', clientId],
     queryFn: () => getOpportunities(clientId),
-    staleTime: 5 * 60 * 1000
+   
   });
 };
