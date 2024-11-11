b0d1343ee6ba23bcaa689b1a6e9b71ee54b26204
jfbenitezz <fjba216@gmail.com>
2024-11-11 17:41:31 -0500

    Changing stale time

--- src/hooks/useGetOpportunities.ts
+++ src/hooks/useGetOpportunities.ts
@@ -5,6 +5,5 @@ export const useGetOpportunities = (clientId?: number) => {
   return useQuery({
     queryKey: ['opportunities', clientId],
     queryFn: () => getOpportunities(clientId),
-    staleTime: 5 * 60 * 1000
   });
 };
