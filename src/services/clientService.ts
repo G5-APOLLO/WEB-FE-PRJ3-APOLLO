302512be4e1d79182f676e870b3b914aec56ddea
Maria Del Toro <Mdeltoroa@uninorte.edu.co>
2024-11-17 21:02:21 -0500

    detail associated Contacts

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -1,8 +1,7 @@
 export const fetchClientById = async (clientId: number) => {
-    const response = await fetch(`https://web-fe-prj3-api-apollo.onrender.com/clients/${clientId}`);
-    if (!response.ok) {
-      throw new Error('Error al cargar la información del cliente');
-    }
-    return response.json();
-  };
-  
+  const response = await fetch(`https://web-fe-prj3-api-apollo.onrender.com/clients/${clientId}`);
+  if (!response.ok) {
+    throw new Error('Error al cargar la información del cliente');
+  }
+  return response.json();
+};
